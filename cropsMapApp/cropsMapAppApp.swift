0e1c6eee34111e0f3bd8f830bc00f04298a99242
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-12 22:25:48 +0900

    Initial Commit

---
+++ cropsMapApp/cropsMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cropsMapAppApp.swift
+//  cropsMapApp
+//
+//  Created by 小野寺良太 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct cropsMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
